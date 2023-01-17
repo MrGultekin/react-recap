614c4adf125f286aacef4be1302ab4c4cb266ff6
MrGultekin <gultekin@gmail.com>
2023-01-17 21:25:51 +0100

    categories button added but all button.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import './style.css'
 
-const CATEGORIES = [
-   {name: "technology", color: "#3b82f6"},
-   {name: "science", color: "#16a34a"},
-   {name: "finance", color: "#ef4444"},
-   {name: "society", color: "#eab308"},
-   {name: "entertainment", color: "#db2777"},
-   {name: "health", color: "#14b8a6"},
-   {name: "history", color: "#f97316"},
-   {name: "news", color: "#8b5cf6"},
-];
+
 
 const initialFacts = [
    {
@@ -83,10 +74,30 @@ function NewFactForm() {
    );
 }
 
+const CATEGORIES = [
+   {name: "technology", color: "#3b82f6"},
+   {name: "science", color: "#16a34a"},
+   {name: "finance", color: "#ef4444"},
+   {name: "society", color: "#eab308"},
+   {name: "entertainment", color: "#db2777"},
+   {name: "health", color: "#14b8a6"},
+   {name: "history", color: "#f97316"},
+   {name: "news", color: "#8b5cf6"},
+];
 function CategoryFilter() {
    return (
        <aside>
-          Categories
+          <ul>
+                {CATEGORIES.map((cat) =>  <li className="category">
+                   <button
+                       className="btn btn-category"
+                       style={{backgroundColor: cat.color}}
+                   >
+                      {cat.name}
+                   </button>
+                </li>)}
+
+          </ul>
        </aside>
    );
 }
